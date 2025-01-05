d6303354977568b3d21af9e7741748ceb801208d
Tanish Gupta <56788039+tanish2007@users.noreply.github.com>
2025-01-05 21:05:45 +0530

    Update index.js

--- mg_backend/index.js
+++ mg_backend/index.js
@@ -1,1 +1,27 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const mongoose = require('mongoose')
+const app=express()
 
+const userRoutes= require("./routes/User")
+
+app.use(bodyParser.json()); 
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','GET, POST , PUT,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+    console.log("CORS headers set")
+    next();
+});
+
+app.use(userRoutes)
+
+mongoose.connect("mongodb+srv://tanishgupta2007:Pasta%40nashta1@cluster0.5kri1.mongodb.net/memegen")
+.then(result=>{
+    console.log("MongoDB is connected")
+    app.listen('8000', () => {
+        console.log("Backend is listening at port 8000");
+     });
+})
+.catch(e=> console.log(e)); // error in connecting to mongodb   
