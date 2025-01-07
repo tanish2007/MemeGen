71fe7ca6a837ddc72a1f9e687c26c1637bfea98c
Tanish Gupta <56788039+tanish2007@users.noreply.github.com>
2025-01-07 20:32:39 +0530

    Update User.js

--- mg_backend/routes/User.js
+++ mg_backend/routes/User.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router= express.Router()
 const controller= require('../controller/user')
+const memeController= require('../controller/meme')
 
 router.post('/createUser',controller.createUser)
 router.post('/authenticateUser',controller.authenticateUser)
-router.post('/generateMeme',controller.generateMeme)
+router.post('/generateMeme',memeController.generateMeme)
 
 module.exports= router;
